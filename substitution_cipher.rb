bdbd82378903092ca225d861ec7b7dbdd8e51d4e
Robert-Rino <a901002666@gmail.com>
2016-03-17 15:23:13 +0800

    permutation encript

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -33,7 +33,7 @@ module SubstitutionCipher
         offset = new_order - 32
         offset = offset%95 # every 95 charactor will repeat
         new_ascii = 32+offset
-        puts new_ascii
+        # puts new_ascii
         return new_ascii.chr
       elsif new_order < 32 then
         offset = 126 - new_order
@@ -41,7 +41,7 @@ module SubstitutionCipher
         new_ascii = 126 - offset
         return new_ascii.chr
       end
-      puts new_order
+      # puts new_order
       return new_order.chr
     end
   end
@@ -54,6 +54,20 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      map_table = []
+      for ascii in 0..127
+        map_table << ascii
+      end
+      map_table = map_table.shuffle(random: Random.new(key)) #shuffled map_table
+
+      str_arr = document.to_s.chars
+      str_arr.each_with_index do |c,index|
+        replaced_ascii = map_table[c.ord]
+        replaced_char = replaced_ascii.to_i.chr
+        str_arr[index] = replaced_char
+      end
+
+      return str_arr.join
     end
 
     # Decrypts String document using integer key
@@ -63,6 +77,20 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      map_table = []
+      for ascii in 0..127
+        map_table << ascii
+      end
+      map_table = map_table.shuffle(random: Random.new(key)) #shuffled map_table
+      reverse_hash = Hash[map_table.map.with_index.to_a]
+
+      str_arr = document.to_s.chars
+      str_arr.each_with_index do |c,index|
+        ori_ascii = reverse_hash[c.ord]
+        ori_char = ori_ascii.to_i.chr
+        str_arr[index] = ori_char
+      end
+      return str_arr.join
     end
   end
 end
--- test.rb
+++ test.rb
@@ -4,8 +4,14 @@ require_relative './substitution_cipher'
 # card = CreditCard.new(79927398713, nil, nil, nil)
 # puts card.validate_checksum
 
+#Casear Cipher test
+# enc = SubstitutionCipher::Caesar.encrypt("hello im RINO", 261)
+# puts enc
+# dec = SubstitutionCipher::Caesar.decrypt(enc, 261)
+# puts dec
 
-enc = SubstitutionCipher::Caesar.encrypt("hello im RINO", 261)
-puts enc
-dec = SubstitutionCipher::Caesar.decrypt(enc, 261)
-puts dec
+#Permutation Cipher test
+enc = SubstitutionCipher::Permutation.encrypt("hello im RINO", 1)
+puts enc.to_s
+dec = SubstitutionCipher::Permutation.decrypt(enc, 1)
+puts dec.to_s

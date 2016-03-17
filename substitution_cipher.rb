5b66c066c3f43d58cda3443c82ff02a7f699e6ad
Robert-Rino <a901002666@gmail.com>
2016-03-17 14:14:46 +0800

    caser cipher

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -7,8 +7,12 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
+      char_arr = document.to_s.chars #turn input into string
+      char_arr.each_with_index do |c,index|
+        char_arr[index] = shift_char(c.ord+key)
+      end
+      return char_arr.join
     end
-
     # Decrypts String document using integer key
     # Arguments:
     #   document: String
@@ -16,6 +20,29 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
+      char_arr = document.chars
+      char_arr.each_with_index do |c,index|
+        char_arr[index] = shift_char(c.ord-key)
+      end
+      return char_arr.join
+    end
+
+    #turn ascii number to charactor
+    def self.shift_char(new_order)
+      if new_order > 126
+        offset = new_order - 32
+        offset = offset%95 # every 95 charactor will repeat
+        new_ascii = 32+offset
+        puts new_ascii
+        return new_ascii.chr
+      elsif new_order < 32 then
+        offset = 126 - new_order
+        offset = offset%95
+        new_ascii = 126 - offset
+        return new_ascii.chr
+      end
+      puts new_order
+      return new_order.chr
     end
   end
 
--- test.rb
+++ test.rb
@@ -1,4 +1,11 @@
 require_relative './credit_card'
+require_relative './substitution_cipher'
 
-card = CreditCard.new(79927398713, nil, nil, nil)
-puts card.validate_checksum
+# card = CreditCard.new(79927398713, nil, nil, nil)
+# puts card.validate_checksum
+
+
+enc = SubstitutionCipher::Caesar.encrypt("hello im RINO", 261)
+puts enc
+dec = SubstitutionCipher::Caesar.decrypt(enc, 261)
+puts dec
